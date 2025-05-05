2d4b9d892364d496807620c1eb8e8bfbfcd898c1
Eoic <karolis.strazdas@protonmail.com>
2025-05-06 01:17:27 +0300

    Belt item management and shifting on key press.
    * Manage belt item state - "active", "wrong", "correct".
    * Shift to next item on numpad key press.
    * Cache pi digits to local storage and try to read digits
      from there on subsequent requests.

--- src/app/components/digit-belt/digit-belt.component.ts
+++ src/app/components/digit-belt/digit-belt.component.ts
@@ -1,68 +1,90 @@
+import { CommonModule } from '@angular/common';
 import { Component, ElementRef, AfterViewInit, HostListener, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { PiService } from '../../pi.service';
+
+interface Digit {
+  symbol: string;
+  answer: string;
+  status: 'default' | 'wrong' | 'correct' | 'active';
+}
 
 @Component({
   selector: 'app-digit-belt',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './digit-belt.component.html',
   styleUrl: './digit-belt.component.scss',
 })
 export class DigitBeltComponent implements AfterViewInit {
   @ViewChild('belt', { static: true, }) belt!: ElementRef<HTMLDivElement>;
-  @ViewChild('cellRef', { static: false, }) cellRef!: ElementRef<HTMLElement>;
+  @ViewChild('cell', { static: false, }) cell!: ElementRef<HTMLElement>;
 
-  constructor(private cdRef: ChangeDetectorRef) {}
+  constructor(private cdRef: ChangeDetectorRef, private piService: PiService) {}
 
-  readonly VISIBLE_CELLS = 5;
-  readonly BUFFER_CENTER = Math.floor(this.VISIBLE_CELLS / 2);
+  readonly FETCH_CHUNK_SIZE = 20;
 
-  window: number[] = [];
+  position = 0;
+  window: Digit[] = [];
 
-  globalIndex = 0;
+  ngOnInit() {
+    this.piService.getDigits(1, this.FETCH_CHUNK_SIZE).subscribe({
+      next: digits => {
+        this.window = digits.split('').map((digit, index) => ({
+          symbol: digit,
+          answer: digit,
+          status: 'default',
+        }));
+      },
+      error: err => {
+        console.error('Failed to fetch Pi digits', err);
+      },
+    });
+  }
 
   ngAfterViewInit() {
     // for (let i = -this.BUFFER_CENTER; i <= this.BUFFER_CENTER; i++) 
-    //   this.window.push(this.globalIndex + 1);
-   
-    // console.log('Initialized...');
-    // this.belt.nativeElement.scrollLeft = this.BUFFER_CENTER * this.CELL_WIDTH;
-
-    for (let i = -this.BUFFER_CENTER; i <= this.BUFFER_CENTER; i++) 
-      this.window.push(this.globalIndex + i);
+    //   this.window.push(this.globalIndex + i);
   
-
-    // trigger manual check so Angular is in sync
     this.cdRef.detectChanges();
 
-    // optionally measure and scroll after a tick
-    setTimeout(() => {
-      const offset = this.getCellWidth() * this.BUFFER_CENTER;
-      this.belt.nativeElement.scrollLeft = offset;
-    });
+    // setTimeout(() => {
+    // const offset = this.getCellWidth() * this.BUFFER_CENTER;
+    // this.belt.nativeElement.scrollLeft = offset;
+    // });
   }
 
-  @HostListener('window:keydown', ['$event'])
-  handleKeyboardEvent(event: KeyboardEvent) {
-    if (event.key === 'Enter')
-      this.shiftLeft();
-    else if (event.key.length === 1 && event.key.match(/[0-9]/)) 
-      console.log(`Digit ${event.key} pressed`);
+  @HostListener('window:keyup', ['$event'])
+  handleKeyUpEvent(event: KeyboardEvent) {
+    if (event.key.length !== 1 || !event.key.match(/[0-9]/))
+      return;
+
+    this.nextDigit(event.key);
   }
 
-  private shiftLeft() {
-    this.window.pop();
-    this.globalIndex--;
-    this.window.unshift(this.globalIndex - this.BUFFER_CENTER);
+  private nextDigit(guess: string) {
+    const poppedDigit = this.window[this.position];
+
+    if (!poppedDigit)
+      return;
+
+    if (guess !== poppedDigit.answer)
+      poppedDigit.status = 'wrong';
+    else poppedDigit.status = 'correct';
+
+    this.position++;
+    // this.window.unshift(this.globalIndex - this.BUFFER_CENTER);
 
     const shift = this.getCellWidth();
-    this.belt.nativeElement.scrollLeft += shift;
-    this.belt.nativeElement.scrollTo({
-      left: this.belt.nativeElement.scrollLeft + shift,
-      behavior: 'smooth',
-    });
+    console.log(this.belt.nativeElement);
+    this.belt.nativeElement.style.left = `${-shift * this.position}px`;
+    // this.belt.nativeElement.scrollTo({
+    //   left: this.belt.nativeElement.scrollLeft + shift,
+    //   behavior: 'smooth',
+    // });
   }
 
   private getCellWidth(): number {
-    const cell = this.cellRef?.nativeElement;
+    const cell = this.cell?.nativeElement;
 
     if (!cell)
       return 0;
--- src/app/pi.service.ts
+++ src/app/pi.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,11 +10,24 @@ export class PiService {
 
   constructor(private http: HttpClient) {}
 
-  getDigits(start: number, numberOfDigits: number): Observable<string> {
+  cacheKey(start: number, numDigits: number): string {
+    return `pi-range_${start}_${numDigits}`;
+  }
+
+  getDigits(start: number, numDigits: number): Observable<string> {
+    const cachedDigits = localStorage.getItem(this.cacheKey(start, numDigits));
+
+    if (cachedDigits) 
+      return new Observable<string>((observer) => {
+        observer.next(cachedDigits);
+        observer.complete();
+      });
+
     return this.http.get<{ content: string }>(
-      `${this.apiUrl}?start=${start}&numberOfDigits=${numberOfDigits}`
-    ).pipe(
-      map(res => res.content)
-    );
+      `${this.apiUrl}?start=${start}&numberOfDigits=${numDigits}`
+    ).pipe(map((response) => { 
+      localStorage.setItem(this.cacheKey(start, numDigits), response.content);   
+      return response.content;
+    }));
   }
 }
